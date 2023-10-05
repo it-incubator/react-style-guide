8931404c071fc595fc2f192262915992e2cba27c
safronman <safronmanbox@gmail.com>
2023-10-05 14:52:53 +0300

    fix: change DownloadLink.tsx

--- components/DownloadLink/DownloadLink.tsx
+++ components/DownloadLink/DownloadLink.tsx
@@ -1,17 +1,25 @@
-export const DownloadLink = ({ href }: { href: string }) => {
+type Props = {
+  href: string
+  title: string
+  description: string
+}
+
+export const DownloadLink = ({ href, title, description }: Props) => {
   return (
     <p style={{ marginTop: 24 }}>
       <a
         id="raw-url"
         download
         href={href}
+        target="_blank"
+        rel="noreferrer"
         className={
           'nx-text-primary-600 nx-underline nx-decoration-from-font [text-underline-position:from-font]'
         }
       >
-        Скачайте файл watchers.xml
-      </a>{' '}
-      и импортируйте его в Settings | Tools | File Watchers
+        {title}
+      </a>
+      {description}
     </p>
   )
 }
