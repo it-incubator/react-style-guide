9b048ee0e85ee7d8ca7adacb318368d9514a3b59
safronman <safronmanbox@gmail.com>
2023-10-13 15:01:47 +0300

    fix: add NODE_ENV to basePath

--- next.config.js
+++ next.config.js
@@ -6,5 +6,5 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
   swcMinify: true,
-  basePath: '/react',
+  basePath: process.env.NODE_ENV === 'development' ? '' : '/react',
 })
